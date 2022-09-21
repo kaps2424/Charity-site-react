a56919d68dec1b23bb8a1a31a9897e980e783095
kaps2424 <kapillay99@gmail.com>
2022-09-21 22:01:50 +0200

    successfully added image scrolling to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,25 @@
 import React, { Component } from "react";
 import axios from 'axios';
+//import { StyleSheet, div } from "react-native";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+//list of images
+const images = [
+    "https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80",
+    "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1429087969512-1e85aab2683d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "https://images.unsplash.com/photo-1505678261036-a3fcc5e884ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+   ];
+
+   //const styles = StyleSheet.create({
+    //container: {
+  //  flex: 1,
+   // backgroundColor: "#fff",
+  //  alignItems: "center",
+  //  justifyContent: "center"
+   // }});
 
 class Home extends Component {
     state = {
@@ -32,11 +52,15 @@ class Home extends Component {
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
+            
+
+                  
                         <div className="card-content" onClick={() => { this.deletePost(post.id) }}>
                             <span className="card-title">{post.title}</span>
                             <p>{post.body}</p>
                         </div>
                     </div>
+                    
                 )
             })
         ) : (
@@ -44,6 +68,20 @@ class Home extends Component {
         )
         return (
             <div className="container">
+                      <Carousel autoPlay = {true} interval = {3000} infiniteLoop = {true}>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
